4dc4b20beb2a99b4a27b0a073c9afc848fbf4a6f
hy <1293360341@qq.com>
2023-03-26 23:12:04 +0800

    1.地域即跨站脚本（XSS）攻击

--- src/main/java/com/example/emos/wx/EmosWxApiApplication.java
+++ src/main/java/com/example/emos/wx/EmosWxApiApplication.java
@@ -3,9 +3,11 @@ package com.example.emos.wx;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.example.emos.wx.db.dao")
+@ServletComponentScan
 public class EmosWxApiApplication {
 
     public static void main(String[] args) {
