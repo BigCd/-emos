16816d3b164073f49015e2b51e9252d8646bc101
hy <1293360341@qq.com>
2023-03-26 22:38:39 +0800

    1.配置后端验证功能

--- src/main/java/com/example/emos/wx/controller/TestController.java
+++ src/main/java/com/example/emos/wx/controller/TestController.java
@@ -1,11 +1,15 @@
 package com.example.emos.wx.controller;
 
 import com.example.emos.wx.common.util.R;
+import com.example.emos.wx.controller.form.TestSayHelloForm;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/test")
@@ -13,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/sayHello")
     @ApiOperation("最简单的测试方法")
-    public R sayHello(){
-        return R.ok().put("message","HelloWorld");
+    public R sayHello(@Valid @RequestBody TestSayHelloForm form){
+        return R.ok().put("message","Hello,"+form.getName());
     }
 }
